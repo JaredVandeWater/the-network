0d2da950e7f9e5a9c75c16892505a7588e0c975c
Jared VandeWater <jaredvande@gmail.com>
2021-04-25 19:01:54 -0600

    1

--- src/components/olderNewer.vue
+++ src/components/olderNewer.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <button @click="getNewer" class="btn btn-info">
+    <button @click="getNewer" class="btn btn-info" v-if="appstate.newer !== null">
       Newer
     </button>
-    <button @click="getOlder" class="btn btn-info">
+    <button @click="getOlder" class="btn btn-info" v-if="appstate.older !== null">
       Older
     </button>
   </div>
@@ -12,11 +12,13 @@
 <script>
 import { postsService } from '../services/PostsService'
 import { AppState } from '../AppState'
+import { computed } from 'vue'
 
 export default {
   name: 'OlderNewer',
   setup() {
     return {
+      appstate: computed(() => AppState),
       async getNewer() {
         try {
           if (AppState.newer !== null) {
